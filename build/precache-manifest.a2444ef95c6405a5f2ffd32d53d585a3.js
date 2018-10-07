af1ae4e8272d1a38d34a0fccbf1f457c85c92ab4
Diana <dianajulaton@gmail.com>
2018-10-06 17:46:34 -0700

    fixing this crap

---
+++ build/precache-manifest.a2444ef95c6405a5f2ffd32d53d585a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1d08fedfe058ca8b230",
+    "url": "/static/js/main.d1d08fed.chunk.js"
+  },
+  {
+    "revision": "f8b378fcb212af62d34c",
+    "url": "/static/js/1.f8b378fc.chunk.js"
+  },
+  {
+    "revision": "d1d08fedfe058ca8b230",
+    "url": "/static/css/main.e4447be4.chunk.css"
+  },
+  {
+    "revision": "f25dd4bad850074184a2a30119f0558f",
+    "url": "/index.html"
+  }
+];
